bd3383f4499849b4efabaa20d8be2a4ddbd9a4b7
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2025-04-09 01:38:08 -0500

    first commit

---
+++ src/main/java/com/taller_springflux/exame_webflux_01/ExamenWebflux01Application.java
@@ -0,0 +1,13 @@
+package com.taller_springflux.exame_webflux_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenWebflux01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenWebflux01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/taller_springflux/exame_webflux_01/ExamenWebflux01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller_springflux.exame_webflux_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenWebflux01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
